fe96525cd0ffbeab7a9fd21018c6cf6f64cbb301
Diana Alvarez <dialmo18@gmail.com>
2025-11-29 20:12:08 -0600

    add all basic folders

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Header from './assets/Components/Header'
+import HeaderOptions from './assets/Components/HeaderOptions'
+
+function App() {
+
+  return (
+    <>
+    <Header>
+      
+    </Header>
+    <HeaderOptions></HeaderOptions>
+
+         
+    </>
+  )
+}
+
+export default App
